df660c69d0cbe6be3a12d08bb7810a6f88289715
Kapindran Kulandaivelu <kapindra@buffalo.edu>
2020-03-24 09:08:29 -0400

    Mar 24 9:08

---
+++ dnademo/Components/NavigationDrawer.js
@@ -0,0 +1,26 @@
+import 'react-native-gesture-handler';
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Alert,
+  } from 'react-native';
+
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import App from '../App';
+
+export const NavDrawer = createDrawerNavigator ();
+
+const styles = StyleSheet.create({
+    analyzeButton: {
+      marginBottom: 15, marginRight: 8, height: 60, width: 200, alignSelf: 'center', backgroundColor: '#ccffdd', justifyContent: 'center', alignItems: 'center', borderRadius: 45,
+    },
+    textFill: {
+        color: 'black', fontWeight: 'bold', fontSize: 15
+    },
+  });
+
+  AppRegistry.registerComponent('NavigationDrawer', () => NavigationDrawer);
